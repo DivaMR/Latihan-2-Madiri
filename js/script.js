7d14296537cc4dbd4c6b34baf480310c1f5b1a18
= <=>
2024-06-13 22:14:22 +0700

    Update terakhit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-// ini js
+function imgSlider (anything) {
+    document.querySelector('.idolq').src = anything;
+
+}
+
+ function changeBgColor(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+ }
